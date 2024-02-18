813cc60ce819a87db020e2059d2cce6866e0a5cc
Armin-FalDiS <fara@odoo.com>
2024-02-18 11:01:57 +0100

    Implement chatter addon

---
+++ armin_chatter/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Armin - Chatter",
+    "version": "17.0.1.0.0",
+    "summary": """Removes the Send Message button from chatter.""",
+    "author": "Armin",
+    "license": "GPL-2",
+    "depends": [
+        "mail"
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "armin_chatter/static/src/core/web/chatter.xml",
+        ]
+    },
+}
